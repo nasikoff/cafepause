9266f7d8c92de7d9f03816f41ba09d38d9c09f9e
rizakhanoff <nf@rizakhanoff.com>
2024-12-12 17:40:19 +0300

    modified:   api/send-order.ts 	new file:   public/img/logodark.png

--- api/send-order.ts
+++ api/send-order.ts
@@ -52,7 +52,7 @@ app.post('/api/send-order', (req: Request, res: Response) => {
     }
     
 
-    const logoUrl = 'https://cafepause.vercel.app/img/logo.png'; 
+    const logoUrl = 'https://cafepause.vercel.app/img/logodark.png'; 
     const orderId = Math.floor(1 + Math.random() * 9000);
     const orderDate = new Date().toLocaleString('ru-RU', {
         year: 'numeric',
@@ -85,7 +85,7 @@ const mailOptions = {
     subject: 'Новый заказ',
     html: `
     <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; background-color: #f7f7f7; color: #333;">
-        <img src="${logoUrl}" alt="Логотип кафе Пауза" style="display: block; margin: 0 auto; max-width: 100%; height: auto;"/>
+        <img src="${logoUrl}" alt="Логотип кафе Пауза" style="width: 150px display: block; margin: 0 auto; max-width: 100%; height: auto;"/>
         <h3>Заказ №${orderId} от ${orderDate}</h3>
         <p><strong>Имя:</strong> ${name}</p>
         <p><strong>Телефон:</strong> ${phone}</p>
